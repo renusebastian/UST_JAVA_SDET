1487443d5b5e4092cca3bdcf052180c9a1000caa
renusebastian <92137654+renusebastian@users.noreply.github.com>
2021-10-22 18:05:19 +0530

    Add files via upload

---
+++ CYPRESS/sample.spec.js
@@ -0,0 +1,8 @@
+describe('simple Test', function () {
+    
+       it('Scenario 1', function (){
+          // test step for URL launching
+          cy.visit("https://www.google.com/");
+       });
+    });
+    
