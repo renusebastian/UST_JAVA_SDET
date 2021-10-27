3e8b778eeabb14f9faaa33a3c263a195a856f360
renusebastian <92137654+renusebastian@users.noreply.github.com>
2021-10-27 12:36:08 +0530

    Add files via upload

---
+++ SELENIUM/cssselector_iframes_2.java
@@ -0,0 +1,38 @@
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class cssselector_iframes_2 {
+	public static WebDriver driver;
+	 
+	 public static void main(String[] args) {
+	 
+	  
+	  System.out.println("Launching the chrome driver  ");  
+	    
+	  // Set the chrome driver exe file path
+	  System.setProperty("webdriver.chrome.driver","E:\\selenium_sumit\\chromedriver_win32_2.36\\chromedriver.exe");  
+	  
+	  // Instantiate the chrome driver
+	  driver = new ChromeDriver();
+	  
+	  // wait time
+	  driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	  
+	  // set the browser URL in get() to load the webpage
+	     driver.get("https://skptricks.github.io/learncoding/selenium-demo/login%20registration%20page/Register.html");  
+	     
+	     // locating an elements using css selector
+	     driver.findElement(By.cssSelector("input.button.style1.style2")).click();
+	     
+
+	 } 
+
+	}
+
+
+
+	
+
